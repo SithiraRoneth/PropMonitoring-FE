0151bef0c5a9b2b5ed448aa110f53a01d316290d
USER <sithiraroneth@gmail.com>
2024-11-09 01:10:48 +0530

    first commit

---
+++ Js/login.js
@@ -0,0 +1,32 @@
+let login = document.getElementById('login-name')
+let signup = document.getElementById('signup-name')
+
+login.style.display = 'block'
+signup.style.display = 'none'
+
+function showSignup() {
+            document.getElementById("loginDiv").style.display = "none";
+            document.getElementById("signupDiv").style.display = "block";
+            signup.style.display='block'
+            login.style.display='none'
+}
+    
+function showLogin() {
+            document.getElementById("signupDiv").style.display = "none";
+            document.getElementById("loginDiv").style.display = "block";
+            login.style.display='block'
+            signup.style.display='none'
+}
+$('#loginbtn').click(function() {
+     window.location.href="index2.html";
+    // document.getElementById('section-login').style.display = 'none'
+    // document.getElementById('dashboard-section').style.display = 'block' 
+});
+
+$('#btnSignup').click(function(){
+    let email = $("#signup-email").val();
+    let pw = $("#signup-password").val();
+    let role = $("#signup-role").val();
+
+    console.log(email,pw,role)
+})
